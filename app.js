5f39a94d14363ec1904fbb92c291f13bc16b6a13
Juhee Kim <sayjh26@gmail.com>
2020-08-22 22:08:39 -0700

    modified sentences and readme file

--- app.js
+++ app.js
@@ -13,55 +13,55 @@ const employees = [];
 const prompts = {
     "Manager": [
         {
-            message: "What is their name?",
+            message: "What is your manager's name?",
             name: "name"
         }, 
         {
-            message: "What is their email?",
+            message: "What is your manager's email?",
             name: "email"
         },
         {
-            message: "What is their id？",
+            message: "What is your manager's id？",
             name: "id"
         },
         {
-            message: "What is their office number?",
+            message: "What is your manager's office number?",
             name: "officeNumber"
         }
     ],
     "Engineer":[
         {
-            message: "What is their name?",
+            message: "What is your engineer's name?",
             name: "name"
         }, 
         {
-            message: "What is their email?",
+            message: "What is your engineer's email?",
             name: "email"
         },
         {
-            message: "What is their id？",
+            message: "What is your engineer's id？",
             name: "id"
         },
         {
-            message: "What is their github?",
+            message: "What is your engineer's GitHub username?",
             name: "github"
         }
     ],
     "Intern":[
         {
-            message: "What is their name?",
+            message: "What is your intern's name?",
             name: "name"
         }, 
         {
-            message: "What is their email?",
+            message: "What is your intern's email?",
             name: "email"
         },
         {
-            message: "What is their id？",
+            message: "What is your intern's id？",
             name: "id"
         },
         {
-            message: "What is their school?",
+            message: "What is your intern's school?",
             name: "school"
         }
     ]
@@ -69,12 +69,12 @@ const prompts = {
 
 function init(){
     inquirer.prompt({
-        message: "Hello who would you like to add to the team?",
+        message: "Please build your team!",
         type: "list",
-        choices: ["Manager", "Engineer", "Intern", "I'm done, create my team."],
+        choices: ["Manager", "Engineer", "Intern", "I don't want to add any more team members."],
         name: "job"
     }).then(({job})=> {
-        if(job === "I'm done, create my team.") {
+        if(job === "I don't want to add any more team members.") {
             //call generate team function with employees array
             console.log(employees);
             fs.writeFile(outputPath, render(employees), err=> console.log(err||"success!"))
@@ -106,25 +106,3 @@ function init(){
 }
 
 init()
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
